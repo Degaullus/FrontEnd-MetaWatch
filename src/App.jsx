02fdebc4c64ec53a2355a6a893884ab5c8182359
Mathieu Deschamps <153502859+Degaullus@users.noreply.github.com>
2024-04-01 09:06:00 +0200

    added Router, Na vBar and Not found tab

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,27 @@ import Format from './components/Format/Format';
 import Homepage from './components/Homepage/Homepage';
 import Location from './components/Location/Location';
 import Navbar from './components/Navbar/Navbar';
+import NotFound from './components/NotFound/NotFound';
 import './App.css';
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
  
 
   return (
-    <>
-     <h1>Meta Watch here</h1>
-     <Navbar />
-      <Homepage />
-      <Faction />
-      <Format />
-      <Location />
-    </>
+  
+  <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/faction" element={<Faction />} />
+      <Route path="/format" element={<Format />} />
+      <Route path="/location" element={<Location />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    </div>
+    
   )
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom"; 
 import styles from "./Navbar.module.css";
 
 export default function Navbar() { 
-
-
     return (
-        <>
-        <h2> Navbar</h2>
-        </>
-    )
-    }
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/" activeclassname={styles.active} ><img src="public/TheOldWorldLogo.png" alt="logoOldworld" /></NavLink>
+                </li>
+                <li>
+                    <NavLink to="/" activeclassname={styles.active} >Homepage</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/faction" activeclassname={styles.active} >Faction</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/format" activeclassname={styles.active} >Format</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/location" activeclassname={styles.active} >Location</NavLink>
+                </li>
+                <li>
+                    <input type="text" name="" id="" placeholder="Searchbar" activeclassname={styles.active} />
+                </li>
+            </ul>
+        </nav>
+    );
+}
