a462e9c648500039c5fdc301b6d93a59a68aae15
Paul Lorber <paul.lorber@protonmail.com>
2024-04-03 01:41:16 +0200

    Add AuthContextProvider to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+
+import AuthContext from './context/authContext.jsx'
 import APIContextProvider from "./context/apiContext.jsx";
+import AuthContextProvider from './components/Authentication/Auth.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-    <APIContextProvider>
-      <App />
-    </APIContextProvider>
+      <AuthContextProvider>
+        <APIContextProvider>
+          <App />
+         </APIContextProvider>
+      </AuthContextProvider>
     </BrowserRouter>
 )
