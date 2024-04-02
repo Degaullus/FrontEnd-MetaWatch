3ce972b9693eec6a49e29eb8e8e5c3e6e55995db
Mathieu Deschamps <153502859+Degaullus@users.noreply.github.com>
2024-04-02 14:23:16 +0200

    add data context

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import Authentication from './components/Authentication/Authentication';
 import NotFound from './components/NotFound/NotFound';
 import './App.css';
 import React from 'react';
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
+import { DataContext } from './context/DataContext';
 
 function App() {
  
@@ -15,15 +16,15 @@ function App() {
   return (
   
   <div>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Homepage />} />
-      <Route path="/faction" element={<Faction />} />
-      <Route path="/format" element={<Format />} />
-      <Route path="/location" element={<Location />} />
-      <Route path="/authentication" element={<Authentication />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/faction" element={<Faction />} />
+          <Route path="/format" element={<Format />} />
+          <Route path="/location" element={<Location />} />
+          <Route path="/authentication" element={<Authentication />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
     </div>
     
   )
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,28 +7,28 @@ export default function Navbar() {
         <nav className={styles.navbarContainer}>
             <ul>
                 <li className={styles.liNavbar}>
-                    <NavLink to="/" activeclassname={styles.active} ><img className={styles.oldWorldLogo} src="public/TheOldWorldLogo.png" alt="logoOldworld" /></NavLink>
+                    <NavLink to="/" activeClassName={styles.active} ><img className={styles.oldWorldLogo} src="public/TheOldWorldLogo.png" alt="logoOldworld" /></NavLink>
                 </li>
                 <li className={styles.liNavbar}>
-                    <NavLink to="/" activeclassname={styles.active} >Homepage</NavLink>
+                    <NavLink to="/" activeClassName={styles.active} >Homepage</NavLink>
                 </li>
                 <li className={styles.liNavbar}>
-                    <NavLink to="/faction" activeclassname={styles.active} >Faction</NavLink>
+                    <NavLink to="/faction" activeClassName={styles.active} >Faction</NavLink>
                 </li>
                 <li className={styles.liNavbar}>
-                    <NavLink to="/format" activeclassname={styles.active} >Format</NavLink>
+                    <NavLink to="/format" activeClassName={styles.active} >Format</NavLink>
                 </li >
                 <li className={styles.liNavbar}>
-                    <NavLink to="/location" activeclassname={styles.active} >Location</NavLink>
+                    <NavLink to="/location" activeClassName={styles.active} >Location</NavLink>
                 </li>
                 <li >
                     <div className="styles.searchBar">
-                    <input type="text" name="" id="" placeholder="Searchbar" activeclassname={styles.active} />
+                    <input type="text" name="" id="" placeholder="Searchbar" activeClassName={styles.active} />
                     <button>Search</button>
                     </div>
                 </li>
                 <li>
-                    <button> <NavLink to="/authentication" activeclassname={styles.active} >Login</NavLink></button>
+                    <button> <NavLink to="/authentication" activeClassName={styles.active} >Login</NavLink></button>
                 </li>
             </ul>
         </nav>
