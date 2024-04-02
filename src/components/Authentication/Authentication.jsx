a01d8004b61947e5392983632643a725c8fde3e9
Paul Lorber <paul.lorber@protonmail.com>
2024-04-02 14:32:11 +0200

    Update Authentication component with login and signup forms

--- src/components/Authentication/Authentication.jsx
+++ src/components/Authentication/Authentication.jsx
@@ -5,7 +5,26 @@ export default function Authentication () {
 
     return (
         <>
-        <h2>Authentication-Login</h2>
+        <h2 className={styles.heading}>Authentication-Site</h2>
+        <div className={styles.container}>
+            <div className={styles.form}>
+                <h3 className={styles.formHeading}>Login</h3>
+                <form>
+                    <input className={styles.input} type="text" placeholder="email" />
+                    <input className={styles.input} type="password" placeholder="Password" />
+                    <button className={styles.button}>Login</button>
+                </form>
+            </div>
+            <div className={styles.form}>
+                <h3 className={styles.formHeading}>Sign up</h3>
+                <form>
+                    <input className={styles.input} type="text" placeholder="email" />
+                    <input className={styles.input} type="password" placeholder="Password" />
+                    <button className={styles.button}>Sign up</button>
+                </form>
+            </div>
+        </div>
+
         </>
     )
     }
