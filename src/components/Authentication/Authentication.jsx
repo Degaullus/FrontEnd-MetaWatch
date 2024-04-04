221bc4065ee5fd087a9ebdf8c36714e1447a2928
Paul Lorber <paul.lorber@protonmail.com>
2024-04-04 18:53:13 +0200

    Refactor authentication component styles

--- src/components/Authentication/Authentication.jsx
+++ src/components/Authentication/Authentication.jsx
@@ -4,6 +4,8 @@ import styles from "./Authentication.module.css";
 export default function Authentication () { 
     
     const [auth, setAuth] = useState("login");
+  
+
 
     const handleAuth = () => {
         if (auth === "login") {
@@ -14,14 +16,16 @@ export default function Authentication () {
     }
 
     useEffect(() => {
-        handleAuth()
+        handleAuth();
     }, [])
 
     return (
         <>
-            <div>
-                <h1>{auth}</h1>
-                <button onClick={handleAuth}>Switch</button>
+            <div className={styles.container}>
+                <h1 className={styles.heading}>{auth}</h1>
+                <input className={styles.input} type="text" placeholder="email" />
+                <input className={styles.input} type="password" placeholder="password" />
+                <button className={styles.switchButton} onClick={handleAuth}>Switch</button>
             </div>
         </>
     )
