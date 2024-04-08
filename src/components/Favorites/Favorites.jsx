7eb7631d32e0a9728fbba47904dbcccbb7270ec2
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 16:20:28 +0200

    Refactor Favorites.jsx to remove unused code and import useWindowSizeContext hook

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,10 +1,6 @@
 import { useWindowSizeContext } from "../../context/WindowSizeContext";
 
 function Favorites () {
-  const size = useWindowSizeContext();
-  console.log(size);
-
-  const { width, height } = useWindowSizeContext();
 
     return (
         <div>
