a0b90f6626639eaf9f2f8a6a789d3ea44056b3d9
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 19:38:17 +0200

    Update LoadingContextProvider import in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import APIContextProvider from "./context/apiContext.jsx";
 import AuthContextProvider from "./context/authContext.jsx";
-import LoadingContextProvider from "./context/LoadingContext.jsx";
+import { LoadingProvider} from "./context/LoadingContext.jsx";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
@@ -12,12 +12,12 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <LoadingContextProvider>
+    <LoadingProvider>
       {/* <AuthContextProvider> */}
         <APIContextProvider>
           <App />
         </APIContextProvider>
       {/* </AuthContextProvider> */}
-    </LoadingContextProvider>
+    </LoadingProvider>
   </BrowserRouter>
 );
