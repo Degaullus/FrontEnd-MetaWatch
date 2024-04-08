a0f43efe618776fdf267f4156a41773f9e63714c
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 13:45:15 +0200

    Refactor useWindowSize hook to handle server-side rendering

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -1,12 +1,17 @@
 import { useState, useEffect } from 'react';
 
+const isSSR = typeof window === 'undefined';
+
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: isSSR ? undefined : window.innerWidth,
+    height: isSSR ? undefined : window.innerHeight,
   });
 
   useEffect(() => {
+
+    if (isSSR) return;
+    
     const handleResize = () => {
       setWindowSize({
         width: window.innerWidth,
