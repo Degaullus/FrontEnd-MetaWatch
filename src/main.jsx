adbe9f6a44dc486e2d722283707c739fe3f7bd66
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 13:40:04 +0200

    Refactor main.jsx to remove AuthContextProvider and update comments

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import APIContextProvider from "./context/apiContext.jsx";
-import AuthContextProvider from "./context/authContext.jsx";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
@@ -11,10 +10,8 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    {/* <AuthContextProvider> */}
       <APIContextProvider>
         <App />
       </APIContextProvider>
-    {/* </AuthContextProvider> */}
   </BrowserRouter>
 );
