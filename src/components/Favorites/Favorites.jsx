12e0672f6dd6d8b57fc3b146eee0e212632d0299
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 13:40:12 +0200

    Refactor Favorites.jsx to use useWindowSizeContext hook

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -1,6 +1,9 @@
 import { useWindowSizeContext } from "../../context/WindowSizeContext";
 
 function Favorites () {
+
+  const { width, height } = useWindowSizeContext();
+  
     return (
         <div>
             <h1>Favorites</h1>
