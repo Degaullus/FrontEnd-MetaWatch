003f23ad3852890ff7a36be2f2ed7e507cab6232
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 13:41:59 +0200

    Refactor main.jsx to add WindowSizeProvider and update comments

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import APIContextProvider from "./context/apiContext.jsx";
+import { WindowSizeProvider } from "./context/WindowSizeContext.jsx";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
@@ -11,7 +12,9 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
       <APIContextProvider>
-        <App />
+        <WindowSizeProvider>
+          <App />
+        </WindowSizeProvider>
       </APIContextProvider>
   </BrowserRouter>
 );
