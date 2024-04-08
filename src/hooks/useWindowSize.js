97e7d993d81e360d63e288d978cb128cab008701
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 16:20:45 +0200

    Refactor useWindowSize hook to handle server-side rendering and add device property

--- src/hooks/useWindowSize.js
+++ src/hooks/useWindowSize.js
@@ -4,16 +4,21 @@ const isSSR = typeof window === 'undefined';
 
 const useWindowSize = () => {
   const [windowSize, setWindowSize] = useState({
+    device: isSSR ? undefined : window.innerWidth,
     width: isSSR ? undefined : window.innerWidth,
     height: isSSR ? undefined : window.innerHeight,
   });
 
+
   useEffect(() => {
 
+
+
     if (isSSR) return;
-    
+
     const handleResize = () => {
       setWindowSize({
+        device: window.innerWidth < 768 ? 'mobile' : 'desktop',
         width: window.innerWidth,
         height: window.innerHeight,
       });
@@ -23,7 +28,7 @@ const useWindowSize = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  return windowSize;
+  return (windowSize);
 }
 
 export default useWindowSize;
