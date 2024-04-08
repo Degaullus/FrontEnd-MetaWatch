ea03e6a17ba9f7f90fa2cd963dd66df9181c885f
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 20:27:52 +0200

    Refactor LoadingBar component to include dynamic width and opacity

--- src/components/Loading/LoadingBar.jsx
+++ src/components/Loading/LoadingBar.jsx
@@ -5,21 +5,40 @@ import style from './Loading.module.css'
 
 const LoadingBar = () => {
   const { isLoading, setIsLoading } = useLoading();
-  const [show, setShow] = useState(false);
+  const [width, setWidth] = useState(0);
+  const [opacity, setOpacity] = useState(1);
 
   useEffect(() => {
-    let timer;
     if (isLoading) {
-      setShow(true);
-    } else {
-      timer = setTimeout(() => setShow(false), 1000);
-    }
-    return () => clearTimeout(timer);
+      setWidth(0);
+      setOpacity(1);
+      setIsLoading(true);
+
+      const interval = setInterval(() => {
+        setWidth((prevWidth) => {
+          if (prevWidth >= 100) {
+            clearInterval(interval);
+            setTimeout(() => setOpacity(0), 700);
+            setTimeout(() => setWidth(0), 600);
+          }
+          return prevWidth + 10;
+        });
+    }, 50);
+  } else {
+    const timeout = setTimeout(() => setWidth(0), 500);
+    return () => clearTimeout(timeout);
+  }
   }, [isLoading]);
 
-  if (!show) return null;
+  if (width <= 0) return null;
 
-  return <div className={style.loadingBar}></div>;
+  return (
+    <div 
+      className={style.loadingBar}  
+      style={{width: `${width}%`, opacity: opacity}}>
+        <h1 className={style.heading}>{width}%</h1>
+    </div>
+);
 }
 
 export default LoadingBar;
