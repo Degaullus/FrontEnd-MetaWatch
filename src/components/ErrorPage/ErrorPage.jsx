eb517d1159dd6c30eb151ad3595953a8728a4d70
Paul Lorber <paul.lorber@protonmail.com>
2024-04-08 20:42:09 +0200

    Add sadFigure image to ErrorPage component

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -5,6 +5,7 @@ export default function ErrorPage() {
     <>
       <div className={styles.warp}>
         <h1>Lost in the warp?</h1>
+        <img className={styles.image}src="images/sadFigure.svg" alt="sad figure" />
       </div>
     </>
   );
