8a003fb66f0caae799802138f91ed0bc08b14ddf
Paul Lorber <paul.lorber@protonmail.com>
2024-04-09 17:36:12 +0200

    Refactor App component to include LoadingSpinner and update loading state in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 //General
 
 import "./App.css";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { useLoading } from "./context/LoadingContext";
 
@@ -23,10 +23,7 @@ function App() {
   const { setIsLoading } = useLoading();
 
   useEffect(() => {
-    // Trigger loading state on route change
-    setIsLoading(true);
-
-    // Simulate loading for at least 0.5 seconds
+   setIsLoading(true);
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 1000);
