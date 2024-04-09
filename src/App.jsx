523ace2b12acf8bcca96e5438a5e0c75537dda07
Paul Lorber <paul.lorber@protonmail.com>
2024-04-09 17:03:54 +0200

    Refactor App component to remove LoadingBar and update routing in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ import Format from "./components/Format/Format";
 import Homepage from "./components/Homepage/Homepage";
 import Location from "./components/Location/Location";
 import Navbar from "./components/Navbar/Navbar";
-import LoadingBar from "./components/Loading/LoadingBar";
 import Authentication from "./components/Authentication/Authentication";
 import Footer from "./components/Footer/Footer";
 import Favorites from "./components/Favorites/Favorites";
@@ -37,19 +36,18 @@ function App() {
 
   return (
     <div>
-      <LoadingBar />
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/faction" element={<Faction />} />
-        <Route path="/faction/factions/:id" element={<FactionSelected />} />
-        <Route path="/format" element={<Format />} />
-        <Route path="/location" element={<Location />} />
-        <Route path="/authentication" element={<Authentication />} />
-        <Route path="/favorites" element={<Favorites />} />
-        <Route path="/error" element={<ErrorPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/faction" element={<Faction />} />
+          <Route path="/faction/factions/:id" element={<FactionSelected />} />
+          <Route path="/format" element={<Format />} />
+          <Route path="/location" element={<Location />} />
+          <Route path="/authentication" element={<Authentication />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/error" element={<ErrorPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
       <Footer />
     </div>
   );
