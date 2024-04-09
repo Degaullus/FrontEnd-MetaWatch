8b21aa6ee2a70d97289ac06e8e50f35bd895d433
Paul Lorber <paul.lorber@protonmail.com>
2024-04-09 22:23:29 +0200

    Refactor authentication logic to include favorite count in authContext.jsx

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -10,20 +10,24 @@ function AuthContextProvider({ children }) {
   const [token, setToken] = useState(null);
   const [email, setEmail] = useState("");
   const [favorites, setFavorites] = useState([]);
+  const [favCount, setFavCount] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
 
   const login = (newToken, details = {}) => {
     setToken(newToken);
     setEmail(details.email);
+    setFavCount(details.favCount || 0);
     setFavorites(details.favorites || []);
     localStorage.setItem('token', newToken);
     localStorage.setItem('email', details.email);
+    localStorage.setItem('favCount', details.favCount || 0);
     localStorage.setItem('favorites', JSON.stringify(details.favorites || []));
   }
 
   const logout = () => {
     setToken(null);
     setEmail("");
+    setFavCount(0);
     setFavorites([]);
     localStorage.clear();
     // localStorage.removeItem('token');
@@ -33,17 +37,17 @@ function AuthContextProvider({ children }) {
   useEffect (() => {
     const storedToken = localStorage.getItem('token');
     const storedEmail = localStorage.getItem('email');
+    const storedFavCount = parseInt(localStorage.getItem('favCount'), 10);
     const storedFavorites = JSON.parse(localStorage.getItem('favorites'));
 
     if (storedToken && storedEmail) {
-      login(storedToken, { email: storedEmail, favorites: storedFavorites });
+      login(storedToken, { email: storedEmail, favCount: storedFavCount, favorites: storedFavorites });
     }
-
     setIsLoading(false);
-  }, []);
+  }, [favCount]);
 
 
-    const value = { token, email, favorites, login,logout};
+    const value = { token, email, favCount, favorites, login,logout};
 
     return (
       <AuthContext.Provider value={value}>
