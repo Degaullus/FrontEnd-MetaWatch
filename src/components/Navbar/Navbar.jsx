848f720145a71eb922d248fb69ecd969376adf50
Paul Lorber <paul.lorber@protonmail.com>
2024-04-11 22:15:23 +0200

    Refactor Navbar component and update styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,26 +18,27 @@ export default function Navbar() {
     navigate("/");
   };
 
-  // Close the hamburger list when clicking outside of it
   useEffect(() => {
     const handleClickOutside = (e) => {
+      if (navbarRef.current && !navbarRef.current.contains(e.target)) {
+        setIsOpen(false);
+      }
+    };
 
-      if (navbarRef.current && !navbarRef.current.contains(e.target)) {setIsOpen(false)}};
-      if (isOpen) {document.addEventListener("mouseup", handleClickOutside)}
-      return () => document.removeEventListener("mouseup", handleClickOutside)
-    }, [isOpen, favCount]);
+    if (isOpen) {
+      document.addEventListener("mouseup", handleClickOutside);
+    }
+    return () => document.removeEventListener("mouseup", handleClickOutside);
+  }, [isOpen]);
 
-  // Open and close the hamburger list
   const toggleMenu = () => setIsOpen((prevState) => !prevState);
 
-  // Close the hamburger list and navigate to the path
   const handleNavigate = (path) => {
     toggleMenu();
     navigate(path);
   };
 
-  // Add the class open to the ulNavbar when the hamburger list is open
-  const ulNavBarClassNames = `${styles.ulNavbar} ${
+  const navContainerClassNames = `${styles.navContainer} ${
     isMobile && isOpen ? styles.open : ""
   }`;
 
@@ -50,48 +51,35 @@ export default function Navbar() {
           alt="logoOldworld"
         />
       </div>
-      <ul className={ulNavBarClassNames}>
-        <li className={styles.liNavbar} onClick={() => handleNavigate("/")}>
+      <div className={navContainerClassNames}>
+        <div className={styles.navItem} onClick={() => handleNavigate("/")}>
           Homepage
-        </li>
-        <li
-          className={styles.liNavbar}
-          onClick={() => handleNavigate("/faction")}
-        >
+        </div>
+        <div className={styles.navItem} onClick={() => handleNavigate("/faction")}>
           Faction
-        </li>
-        <li
-          className={styles.liNavbar}
-          onClick={() => handleNavigate("/format")}
-        >
+        </div>
+        <div className={styles.navItem} onClick={() => handleNavigate("/format")}>
           Format
-        </li>
-        <li
-          className={styles.liNavbar}
-          onClick={() => handleNavigate("/location")}
-        >
+        </div>
+        <div className={styles.navItem} onClick={() => handleNavigate("/location")}>
           Location
-        </li>
-        <li>
-          <div className={styles.searchBar}>
-            <input type="text" placeholder="Searchbar" />
-            <button>Search</button>
-          </div>
-        </li>
+        </div>
+        <div className={styles.searchBar}>
+          <input type="text" placeholder="Searchbar" />
+          <button>Search</button>
+        </div>
         {token ? (
-          <>
-
-            <li className={styles.liNavbar} onClick={handleLogout}>Logout</li>
-            <li className={styles.loggedInAs}>logged in as: {username}</li>
-            <li className={styles.liNavbar} onClick={() => handleNavigate("/favorites")}>{favCount}<img className={styles.favoriteImg}src="/favorite.svg" /></li>
-          </>
+          <div className={styles.profileContainer}>
+            <div className={styles.loggedInAs}>Logged in as: {username}</div>
+            <div className={styles.favorites} onClick={() => handleNavigate("/favorites")}>
+              {favCount}<img className={styles.favoriteImg} src="/favorite.svg" />
+            </div>
+            <div className={styles.logout} onClick={handleLogout}>Logout</div>
+          </div>
         ) : (
-        <>
-          <li className={styles.liNavbar} onClick={() => handleNavigate("/authentication")}>Login</li>
-          </>
-
+          <div className={styles.navItem} onClick={() => handleNavigate("/authentication")}>Login</div>
         )}
-      </ul>
+      </div>
       {isMobile && (
         <div className={styles.hamburger} onClick={toggleMenu}>
           <img src="/hamburger.svg" alt="Menu" />
