66a1e47bfa0f9b0cc1fbfca153f20225f8e5479a
Paul Lorber <paul.lorber@protonmail.com>
2024-04-16 03:19:29 +0200

    Refactor Favorites component to remove unused code and update UI

--- src/components/Favorites/Favorites.jsx
+++ src/components/Favorites/Favorites.jsx
@@ -4,11 +4,9 @@ import { useContext } from "react";
 import style from "./Favorites.module.css";
 
 import { useWindowSizeContext } from "../../context/WindowSizeContext";
-import { favTournamentContext } from "../../context/tournamentsObjContext";
 import { useActions } from '../../context/ActionsContext'; // Assuming ActionsContext is properly exported
 
 export default function Favorites () {
-    const { tournaments } = useContext(favTournamentContext);
     const { removeFavorite } = useActions();
 
     const handleRemoveFavorite = (tournamentId) => {
@@ -20,17 +18,7 @@ export default function Favorites () {
         <>
             <h1>Favorites</h1>
             <div className={style.favoritesContainer}>
-            {tournaments.length > 0 ? (
-                tournaments.map((tournament) => (
-                    <div className={style.tournament} key={tournament._id}>
-                        <h2 className={style.tournamentH2}>{tournament.army}</h2>
-                        <p className={style.tournamentP}>{tournament.list}</p>
-                        <button className={style.tournamentButton} onClick={() => handleRemoveFavorite(tournament._id)}>Remove</button>
-                    </div>
-                ))
-            ) : (
-                <p>No favorites yet</p>
-            )}
+                some favorites
             </div>
         </>
     );
