d04b66251102d5a0ce18222f19e4db9e109a80c9
Paul Lorber <paul.lorber@protonmail.com>
2024-04-16 10:08:48 +0200

    Refactor Navbar component to remove unused code and update authentication handling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useContext } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./Navbar.module.css";
 import SearchBar from "../SearchBar/SearchBar";
@@ -11,19 +11,69 @@ export default function Navbar() {
   const navbarRef = useRef(null);
   const navigate = useNavigate();
   const [token, setToken] = useState(localStorage.getItem('token') || null);
+  const [username, setUsername] = useState("");
+  const [favCount, setFavCount] = useState(0);
+  const backendURL = 'https://backend-metawatch.onrender.com/';
+  const localURL = 'http://localhost:8080/';
+
+  useEffect(() => {
+    const userId = localStorage.getItem('userId');
+    fetchUserDetails(userId, token);
+}, [token, username]);
+
+useEffect(() => {
+    const handleStorageChange = (e) => {
+        if (e.key === 'token') {
+            setToken(localStorage.getItem('token'));
+        }
+        if (e.key === 'userId' && localStorage.getItem('token')) {
+            fetchUserDetails(localStorage.getItem('userId'), localStorage.getItem('token'));
+        }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    return () => {
+        window.removeEventListener('storage', handleStorageChange);
+    };
+}, []);
+
+const fetchUserDetails = async (userId, token) => {
+  if (userId && token) {
+      try {
+          const url = `${localURL}users/${userId}`;
+          const response = await fetch(url, {
+              method: 'GET',
+              headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${token}`
+              }
+          });
+          if (!response.ok) {
+              throw new Error('Failed to fetch user details');
+          }
+          const data = await response.json();
+          console.log(data)
+          setUsername(data.username);
+          setFavCount(data.favCount);
+      } catch (error) {
+          console.error('Error fetching user details:', error);
+      }
+  }
+};
 
   const handleLogout = () => {
     setToken(null);
     localStorage.removeItem('token');
-    navigate('/login');
+    navigate('/');
+};
+  const handleClickOutside = (e) => {
+    if (navbarRef.current && !navbarRef.current.contains(e.target)) {
+      setIsOpen(false);
+    }
 };
 
   useEffect(() => {
-    const handleClickOutside = (e) => {
-      if (navbarRef.current && !navbarRef.current.contains(e.target)) {
-        setIsOpen(false);
-      }
-    };
     if (isOpen) {
       document.addEventListener("mouseup", handleClickOutside);
     }
@@ -51,28 +101,13 @@ export default function Navbar() {
         />
       </div>
       <div className={navContainerClassNames}>
-        <div className={styles.navItem} onClick={() => handleNavigate("/")}>
-          Homepage
-        </div>
-        <div
-          className={styles.navItem}
-          onClick={() => handleNavigate("/faction")}
-        >
-          Faction
-        </div>
-        <div
-          className={styles.navItem}
-          onClick={() => handleNavigate("/format")}
-        >
-          Format
-        </div>
+        <div className={styles.navItem} onClick={() => handleNavigate("/")}>Homepage</div>
+        <div className={styles.navItem} onClick={() => handleNavigate("/faction")}>Faction</div>
+        <div className={styles.navItem} onClick={() => handleNavigate("/format")}>Format</div>
         <SearchBar />
         {token ? (
           <div className={styles.profileContainer}>
-            <div
-              className={styles.favorites}
-              onClick={() => handleNavigate("/favorites")}
-            >
+            <div className={styles.favorites} onClick={() => handleNavigate("/favorites")}>
               <div className={styles.loggedInAs}>
                 <span className={styles.logged}></span>
                 <span className={styles.inas}>{username}</span>
