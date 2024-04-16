d77e8e006288059effb4ad505f4afa97ff3d9c81
Moritz Rentrop <moody@MacBook-Pro-von-Moritz.local>
2024-04-17 01:43:28 +0200

    Fix: Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,45 @@
 import styles from "./Footer.module.css";
 
 function Footer() {
-  
-  const facebookURL = "https://facebook.com";
-  const discordURL = "https://discord.com";
-  const instagramURL = "https://instagram.com";
+  const discordURL = "https://discord.gg/Vmwh48JDzZ";
+  const instagramURL = "https://www.instagram.com/metahammerofficial/";
 
   return (
-
     <footer className={styles.container}>
-      <div className={styles.footerIcons}>
-        <a href={facebookURL}><img className={styles.icon} src="/facebook.svg" alt="Facebook" /></a>
-        <a href={discordURL}><img className={styles.icon} src="/discord.svg" alt="Discord" /></a>
-        <a href={instagramURL}><img className={styles.icon} src="/instagram.svg" alt="Instagram" /></a>
+      <div className={styles.containerTop}>
+        <div className={styles.footerSectionLinks}>
+          <h5>Quick Links</h5>
+
+          <a href="/about">About Us</a>
+
+          <a href="/support">Support</a>
+        </div>
+        <div className={styles.footerSectionCont}>
+          <h5>Contact</h5>
+
+          <div className={styles.footerIcons}>
+            <a href="https://discord.gg/Vmwh48JDzZ">
+              <img className={styles.icon} src="/discord.svg" alt="Discord" />
+            </a>
+            <a href="https://www.instagram.com/metahammerofficial/">
+              <img
+                className={styles.icon}
+                src="/instagram.svg"
+                alt="Instagram"
+              />
+            </a>
+          </div>
+          <p>
+            metahammer.contact@gmail.com <br /> &copy; 2024 MetaHammer. All
+            rights reserved.
+          </p>
+        </div>
+        <div className={styles.footerSectionLinks}>
+          <h5>Legal</h5>
+          <a href="/privacy-policy">Privacy Policy</a>
+          <a href="/terms-of-use">Terms of Use</a>
+        </div>
       </div>
-      <h6>Some rights reserved</h6>
     </footer>
   );
 }
