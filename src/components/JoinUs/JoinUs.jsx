9a15a37ea0a0ab57c41e0eadbccdf0119d3c9860
Degaullus <mathieu.francois.deschamps@gmail.com>
2024-05-26 10:44:37 +0200

    fix design 1st section homepage

--- src/components/JoinUs/JoinUs.jsx
+++ src/components/JoinUs/JoinUs.jsx
@@ -5,11 +5,9 @@ export default function JoinUs() {
     <div className={style.JoinusContainer}>
       <div className={style.title}>
         <h2 className={style.join}>Join us</h2>
-        <a href="https://discord.gg/Vmwh48JDzZ">
-          <img className={style.icon} src="/discord.svg" alt="Discord" />
-        </a>
-        <a href="https://www.instagram.com/metahammerofficial/">
-          <img className={style.icon} src="/instagram.svg" alt="Instagram" />
+
+        <a className={style.mailTo} href="mailto:MetaHammer.contact@gmail.com">
+          MetaHammer.contact@gmail.com
         </a>
       </div>
 
@@ -17,10 +15,14 @@ export default function JoinUs() {
         MetaHammer is still in Beta and need your help. Please report bugs or
         missing tournament results.
       </p>
-
-      <a className={style.mailTo} href="mailto:MetaHammer.contact@gmail.com">
-        MetaHammer.contact@gmail.com
-      </a>
+      <div className={style.social}>
+        <a href="https://discord.gg/Vmwh48JDzZ">
+          <img className={style.icon} src="/discord.svg" alt="Discord" />
+        </a>
+        <a href="https://www.instagram.com/metahammerofficial/">
+          <img className={style.icon} src="/instagram.svg" alt="Instagram" />
+        </a>
+      </div>
     </div>
   );
 }
